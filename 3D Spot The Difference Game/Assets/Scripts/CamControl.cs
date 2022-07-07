7178a955ff300d9139d94065771aa84f5cc62aa3
Kostas Vs <kostasvs@live.com>
2022-07-08 01:51:20 +0300

    modify visuals, camera turn/zoom

---
+++ 3D Spot The Difference Game/Assets/Scripts/CamControl.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts {
+	public class CamControl : MonoBehaviour {
+
+		private PanAndZoom panAndZoom;
+
+		public Camera[] cams = new Camera[2];
+		private readonly Transform[] pivots = new Transform[2];
+		private readonly Transform[] tilts = new Transform[2];
+		private float tiltRot = 0;
+		private float pivotRot = 150;
+		public float tiltRotMin = -30;
+		public float tiltRotMax = 15;
+
+		private float fov;
+		public float fovMin = 24f;
+		public float fovMax = 45f;
+		public float fovSensLerp = 1f;
+
+		public Vector2 turnSens = new Vector2 (.5f, .5f);
+		private Vector2 turnMomentum;
+		private Vector2 turnMomentumDelta;
+		[SerializeField]
+		private float turnMomentumDecay = .4f;
+
+		void Awake () {
+
+			panAndZoom = GetComponent<PanAndZoom> ();
+			panAndZoom.onStartTouch += PanAndZoom_onStartTouch;
+			panAndZoom.onEndTouch += PanAndZoom_onEndTouch;
+			panAndZoom.onTap += PanAndZoom_onTap;
+			panAndZoom.onSwipe += PanAndZoom_onSwipe;
+			panAndZoom.onPinch += PanAndZoom_onPinch;
+
+			for (int i = 0; i < 2; i++) {
+				tilts[i] = cams[i].transform.parent;
+				pivots[i] = tilts[i].parent;
+			}
+			fov = cams[0].fieldOfView;
+		}
+
+		private void PanAndZoom_onPinch (float distOld, float distNew, Vector2 pos, Vector2 delta) {
+
+			fov = Mathf.Clamp (fov * Mathf.Lerp (1f, distOld / distNew, fovSensLerp), fovMin, fovMax);
+			foreach (var c in cams) {
+				c.fieldOfView = fov;
+			}
+		}
+
+		private void PanAndZoom_onSwipe (Vector2 delta) {
+			RotateFromScreenDelta (delta);
+		}
+
+		private void PanAndZoom_onTap (Vector2 pos) {
+		}
+
+		private void PanAndZoom_onEndTouch (Vector2 pos, Vector2 vel) {
+			turnMomentum = vel;
+			turnMomentumDelta = Vector2.zero;
+		}
+
+		private void PanAndZoom_onStartTouch (Vector2 pos) {
+			turnMomentum = Vector2.zero;
+			turnMomentumDelta = Vector2.zero;
+		}
+
+		private void RotateFromScreenDelta (Vector2 delta) {
+
+			pivotRot = (pivotRot + delta.x * turnSens.x) % 360f;
+			tiltRot = Mathf.Clamp (tiltRot + delta.y * turnSens.y, tiltRotMin, tiltRotMax);
+
+			var rot = Quaternion.Euler (0, pivotRot, 0);
+			foreach (var p in pivots) {
+				p.localRotation = rot;
+			}
+
+			rot = Quaternion.Euler (tiltRot, 0, 0);
+			foreach (var t in tilts) {
+				t.localRotation = rot;
+			}
+		}
+
+		void Update () {
+
+			if (turnMomentum != Vector2.zero) {
+
+				RotateFromScreenDelta (turnMomentum);
+				turnMomentum = Vector2.SmoothDamp (turnMomentum, Vector2.zero, ref turnMomentumDelta, turnMomentumDecay);
+			}
+		}
+	}
+}
---
+++ 3D Spot The Difference Game/Assets/Scripts/PanAndZoom.cs
@@ -0,0 +1,210 @@
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+
+/// <summary> A modular and easily customisable Unity MonoBehaviour for handling swipe and pinch motions on mobile. </summary>
+public class PanAndZoom : MonoBehaviour {
+
+	/// <summary> Called as soon as the player touches the screen. The argument is the screen position. </summary>
+	public event Action<Vector2> onStartTouch;
+	/// <summary> Called as soon as the player stops touching the screen. The argument is the screen position, and screen velocity on release. </summary>
+	public event Action<Vector2, Vector2> onEndTouch;
+	/// <summary> Called if the player completed a quick tap motion. The argument is the screen position. </summary>
+	public event Action<Vector2> onTap;
+	/// <summary> Called if the player swiped the screen. The argument is the screen movement delta. </summary>
+	public event Action<Vector2> onSwipe;
+	/// <summary> Called if the player pinched the screen. The arguments are the distance between the fingers before and after, the end screen position, and screen movement delta. </summary>
+	public event Action<float, float, Vector2, Vector2> onPinch;
+
+	[Header ("Tap")]
+	[Tooltip ("The maximum movement for a touch motion to be treated as a tap")]
+	public float maxDistanceForTap = 40;
+	[Tooltip ("The maximum duration for a touch motion to be treated as a tap")]
+	public float maxDurationForTap = 0.4f;
+
+	[Header ("Desktop debug")]
+	[Tooltip ("Use the mouse on desktop?")]
+	public bool useMouse = true;
+	[Tooltip ("The simulated pinch speed using the scroll wheel")]
+	public float mouseScrollSpeed = 2;
+
+	[Header ("UI")]
+	[Tooltip ("Are touch motions listened to if they are over UI elements?")]
+	public bool ignoreUI = false;
+
+	Vector2 touch0StartPosition;
+	Vector2 touch0LastPosition;
+	float touch0StartTime;
+
+	Vector2 momentum;
+	Vector2 momentumLerpSpeed;
+	public float momentumLerpTime = 0.025f;
+
+	bool canUseMouse;
+
+	/// <summary> Has the player at least one finger on the screen? </summary>
+	public bool isTouching { get; private set; }
+
+	/// <summary> The point of contact if it exists in Screen space. </summary>
+	public Vector2 touchPosition { get { return touch0LastPosition; } }
+
+	void Start () {
+		canUseMouse = Application.platform != RuntimePlatform.Android && Application.platform != RuntimePlatform.IPhonePlayer && Input.mousePresent;
+	}
+
+	void Update () {
+		if (useMouse && canUseMouse) {
+			UpdateWithMouse ();
+		}
+		else {
+			UpdateWithTouch ();
+		}
+	}
+
+	void UpdateWithMouse () {
+		if (Input.GetMouseButtonDown (0)) {
+			if (ignoreUI || !IsPointerOverUIObject ()) {
+				touch0StartPosition = Input.mousePosition;
+				touch0StartTime = Time.time;
+				touch0LastPosition = touch0StartPosition;
+
+				isTouching = true;
+
+				onStartTouch?.Invoke (Input.mousePosition);
+			}
+		}
+
+		if (Input.GetMouseButton (0) && isTouching) {
+			Vector2 move = (Vector2)Input.mousePosition - touch0LastPosition;
+			touch0LastPosition = Input.mousePosition;
+			momentum = Vector2.SmoothDamp (momentum, move, ref momentumLerpSpeed, momentumLerpTime);
+
+			if (move != Vector2.zero) {
+				OnSwipe (move);
+			}
+		}
+
+		if (Input.GetMouseButtonUp (0) && isTouching) {
+
+			if (Time.time - touch0StartTime <= maxDurationForTap
+			   && Vector2.Distance (Input.mousePosition, touch0StartPosition) <= maxDistanceForTap) {
+				OnClick (Input.mousePosition);
+			}
+
+			onEndTouch?.Invoke (Input.mousePosition, momentum);
+			isTouching = false;
+			momentum = Vector2.zero;
+			momentumLerpSpeed = Vector2.zero;
+		}
+
+		if (Input.mouseScrollDelta.y != 0) {
+			OnPinch (Input.mousePosition, 1, Input.mouseScrollDelta.y < 0 ? (1 / mouseScrollSpeed) : mouseScrollSpeed, Vector2.right);
+		}
+	}
+
+	void UpdateWithTouch () {
+		int touchCount = Input.touches.Length;
+
+		if (touchCount == 1) {
+			Touch touch = Input.touches[0];
+
+			switch (touch.phase) {
+				case TouchPhase.Began: {
+						if (ignoreUI || !IsPointerOverUIObject ()) {
+							touch0StartPosition = touch.position;
+							touch0StartTime = Time.time;
+							touch0LastPosition = touch0StartPosition;
+
+							isTouching = true;
+
+							onStartTouch?.Invoke (touch0StartPosition);
+						}
+
+						break;
+					}
+				case TouchPhase.Moved: {
+						touch0LastPosition = touch.position;
+
+						if (isTouching) {
+							if (touch.deltaPosition != Vector2.zero) {
+								OnSwipe (touch.deltaPosition);
+							}
+							momentum = Vector2.SmoothDamp (momentum, touch.deltaPosition, ref momentumLerpSpeed, momentumLerpTime);
+						}
+						break;
+					}
+				case TouchPhase.Ended: {
+						if (Time.time - touch0StartTime <= maxDurationForTap
+							&& Vector2.Distance (touch.position, touch0StartPosition) <= maxDistanceForTap
+							&& isTouching) {
+							OnClick (touch.position);
+						}
+
+						onEndTouch?.Invoke (touch.position, momentum);
+						momentum = Vector2.zero;
+						momentumLerpSpeed = Vector2.zero;
+						isTouching = false;
+						break;
+					}
+				case TouchPhase.Stationary:
+				case TouchPhase.Canceled:
+					break;
+			}
+		}
+		else if (touchCount == 2) {
+			Touch touch0 = Input.touches[0];
+			Touch touch1 = Input.touches[1];
+
+			if (touch0.phase == TouchPhase.Ended || touch1.phase == TouchPhase.Ended) return;
+
+			isTouching = true;
+
+			Vector2 prev0 = touch0.position - touch0.deltaPosition;
+			Vector2 prev1 = touch1.position - touch1.deltaPosition;
+
+			float previousDistance = Vector2.Distance (prev0, prev1);
+			float currentDistance = Vector2.Distance (touch0.position, touch1.position);
+
+			Vector2 center = (touch0.position + touch1.position) * .5f;
+			Vector2 centerPrev = (prev0 + prev1) * .5f;
+			if (previousDistance != currentDistance || center != centerPrev) {
+				OnPinch (center, previousDistance, currentDistance, center - centerPrev);
+			}
+			momentum = Vector2.SmoothDamp (momentum, center - centerPrev, ref momentumLerpSpeed, momentumLerpTime);
+		}
+		else {
+			if (isTouching) {
+				onEndTouch?.Invoke (touch0LastPosition, momentum);
+				momentum = Vector2.zero;
+				momentumLerpSpeed = Vector2.zero;
+				isTouching = false;
+			}
+		}
+	}
+
+	void OnClick (Vector2 position) {
+		if (onTap != null && (ignoreUI || !IsPointerOverUIObject ())) {
+			onTap (position);
+		}
+	}
+
+	void OnSwipe (Vector2 deltaPosition) {
+		onSwipe?.Invoke (deltaPosition);
+	}
+
+	void OnPinch (Vector2 center, float oldDistance, float newDistance, Vector2 touchDelta) {
+		onPinch?.Invoke (oldDistance, newDistance, center, touchDelta);
+	}
+
+	/// <summary> Checks if the the current input is over canvas UI </summary>
+	public bool IsPointerOverUIObject () {
+
+		if (EventSystem.current == null) return false;
+		PointerEventData eventDataCurrentPosition = new PointerEventData (EventSystem.current);
+		eventDataCurrentPosition.position = new Vector2 (Input.mousePosition.x, Input.mousePosition.y);
+		List<RaycastResult> results = new List<RaycastResult> ();
+		EventSystem.current.RaycastAll (eventDataCurrentPosition, results);
+		return results.Count > 0;
+	}
+}
