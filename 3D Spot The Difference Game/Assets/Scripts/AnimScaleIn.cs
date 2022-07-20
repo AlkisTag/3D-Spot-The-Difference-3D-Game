7ef3b5632b81ca999b0e820194edd35f2b82ff69
Kostas Vs <kostasvs@live.com>
2022-07-20 21:26:45 +0300

    diff mark scale-in animation

---
+++ 3D Spot The Difference Game/Assets/Scripts/AnimScaleIn.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts {
+	public class AnimScaleIn : MonoBehaviour {
+
+		[SerializeField]
+		private AnimationCurve scaleCurve;
+		[SerializeField]
+		private float animDur = .6f;
+		private float timer;
+
+		private Vector3 initScale;
+
+		void Awake () {
+
+			initScale = transform.localScale;
+			UpdateAnim ();
+		}
+
+		void Update () {
+
+			timer += Time.deltaTime;
+			UpdateAnim ();
+			if (timer >= animDur) enabled = false;
+		}
+
+		private void UpdateAnim () {
+
+			var s = scaleCurve.Evaluate (Mathf.Clamp01 (timer / animDur));
+			transform.localScale = initScale * s;
+		}
+	}
+}
